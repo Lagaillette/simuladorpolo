aa160623d0e5e4c683a73cbda29e056a76821931
Filip <Filip.wasilewski96@gmail.com>
2017-11-07 14:02:16 +0100

    gui v2

--- src/GUIClass.java
+++ src/GUIClass.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.*;
 
 public class GUIClass  {
@@ -54,7 +55,7 @@ public class GUIClass  {
 	    
 	    JButton B_CreaElPolo=new JButton("CreaElPolo");  
 		B_CreaElPolo.setBounds(300,100,200,30);  
-	    window.add(B_CreaElPolo);  
+	   
 	    
 	    B_CreaElPolo.addActionListener(new ActionListener(){  
 	    	public void actionPerformed(ActionEvent e){  
@@ -72,8 +73,8 @@ public class GUIClass  {
 	    
 	    //BUTTON B_Busques------------------------------------------------------------
 	    JButton B_Busques = new JButton("capturas de buques furtivos");
-	   B_Busques.setBounds(50,200,200,30);  
-	    window.add(B_Busques);  
+	  // B_Busques.setBounds(50,200,200,30);  
+	     
 	    
 	    B_Busques.addActionListener(new ActionListener(){  
 	    	public void actionPerformed(ActionEvent e){
@@ -92,8 +93,8 @@ public class GUIClass  {
 	    
 	  //BUTTON 10 dias siguiente------------------------------------------------------------
 	    JButton B_sig10Dias=new JButton("10 dias");  
-	    B_sig10Dias.setBounds(50,200,200,30);  
-	    window.add(B_sig10Dias);  
+	   // B_sig10Dias.setBounds(50,200,200,30);  
+	     
 	    
 	    B_sig10Dias.addActionListener(new ActionListener(){  
 	    	public void actionPerformed(ActionEvent e){  
@@ -119,8 +120,8 @@ public class GUIClass  {
 	    
 	  //BUTTON B_Calentamiento Global------------------------------------------------------------
 	    JButton B_Calentamiento=new JButton("B_Calentamiento Global");  
-	    B_Calentamiento.setBounds(50,300,200,30);  
-	    window.add(B_Calentamiento);  
+	    //B_Calentamiento.setBounds(50,300,200,30);  
+	      
 	    
 	    B_Calentamiento.addActionListener(new ActionListener(){  
 	    	public void actionPerformed(ActionEvent e){  
@@ -138,8 +139,8 @@ public class GUIClass  {
 	    
 	  //BUTTON B_Detalles------------------------------------------------------------
 	    JButton B_Detalles=new JButton("Mostrar B_Detalles");  
-	    B_Detalles.setBounds(50,400,200,30);  
-	    window.add(B_Detalles);  
+	    //B_Detalles.setBounds(50,400,200,30);  
+	    
 	    
 	    B_Detalles.addActionListener(new ActionListener(){  
 	    	public void actionPerformed(ActionEvent e){  
@@ -150,9 +151,15 @@ public class GUIClass  {
 	    
 	  //BUTTON Salida------------------------------------------------------------
 	    JButton B_Salida=new JButton("Salida del programa");  
-	    B_Salida.setBounds(300,50,200,30);  
-	    window.add(B_Salida);  
+	   // B_Salida.setBounds(300,50,200,30);  
 	    
+	    window.add(B_CreaElPolo);  
+	    window.add(B_Busques); 
+	    window.add(B_sig10Dias); 
+	    window.add(B_Calentamiento);
+	    window.add(B_Detalles);
+	    window.add(B_Salida); 
+	    window.setLayout(new GridLayout(3,2));
 	}
 	
 	
